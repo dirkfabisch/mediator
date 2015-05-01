709af4560000101e4f297329650376345471423c
Jacob Turner <poppa.bear.joe@gmail.com>
2015-04-30 22:42:33 -0600

    Update index.js
    Fixes #35

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
-            if($(this).attr("alt") && !(this).hasClass("emoji"))
+            if($(this).attr("alt") && !$(this).hasClass("emoji"))
               $(this).wrap('<figure class="image"></figure>')
               .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
